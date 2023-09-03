93de5154b20848d07f9b3fdc8d92c7e6ac1268a6
vfa-tienld <tienld@gmail.com>
2023-09-03 13:57:04 +0700

    update product dto

--- src/modules/products/dto/product.dto.ts
+++ src/modules/products/dto/product.dto.ts
@@ -1,18 +1,18 @@
 import { IsNotEmpty, MaxLength } from 'class-validator';
 
 export class ProductDto {
-  @IsNotEmpty({ message: 'product name is not empty' })
+  @IsNotEmpty({ message: 'product name is not empty !' })
   productName: string;
 
-  @IsNotEmpty({ message: 'product type is not empty' })
+  @IsNotEmpty({ message: 'product type is not empty !' })
   productType: string;
 
-  @IsNotEmpty({ message: 'url is not empty' })
+  @IsNotEmpty({ message: 'url is not empty !' })
   urlImg: string;
 
-  @MaxLength(80, { message: 'Description the length invalid' })
+  @MaxLength(80, { message: 'Description the length invalid !' })
   description?: string;
 
-  @IsNotEmpty({ message: 'price  is not empty' })
+  @IsNotEmpty({ message: 'price  is not empty !' })
   price: number;
 }
